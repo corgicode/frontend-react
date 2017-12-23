92722b914e924826223987bad27244e8f00d647c
David Silva <david.silva@dokbid.com>
2017-12-23 01:08:46 -0500

    Added redux-json-api with temp base Url

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import store, { history } from './store';
+import { setAxiosConfig } from 'redux-json-api';
 import App from './containers/app';
 
 import 'sanitize.css/sanitize.css';
 import './styles/main.scss';
 
 const target = document.querySelector('#root');
+
+store.dispatch(setAxiosConfig({
+    baseURL: 'http://localhost:9500/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+}));
 
 render(
     <Provider store={store}>
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import counter from './counter';
+import { reducer as api } from 'redux-json-api';
 
 export default combineReducers({
     router: routerReducer,
-    counter
+    api,
 });
