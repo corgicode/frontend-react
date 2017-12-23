4ab07a0af193768525ef97cc3b8598f83f23d357
David Silva <david.silva@dokbid.com>
2017-12-23 14:47:24 -0500

    created routes file to clean app container, requests profile info on first visit

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { reducer as api } from 'redux-json-api';
+import profileReducer from './profile';
 
 export default combineReducers({
     router: routerReducer,
     api,
+    profile: profileReducer,
 });
