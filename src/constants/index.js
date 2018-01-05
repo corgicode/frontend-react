c8f8646c20ac8883a878be481b7e82cbf5cc21f4
David Silva <david.silva@dokbid.com>
2018-01-05 13:44:52 -0500

    Added Helmet to the profile page

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const  PROD_URL =  process.env.PROD_URL || 'https://codecorgi.co';
+
+export const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:9500';
--- src/containers/signup/index.js
+++ src/containers/signup/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import blueBg from '../../assets/images/blue-bg.jpg';
 import femaleOctocat from '../../assets/images/femalecodertocat.png';
 import { Link } from 'react-router-dom';
+import { BACKEND_URL, PROD_URL } from '../../constants';
+import { Helmet } from 'react-helmet';
 
 class Signup extends React.Component {
     render() {
         return(
             <section id="hero" className="dark-bg img-bg img-bg-soft" style={{ backgroundImage: `url(${blueBg})` }}>
+                <Helmet>
+                    <title>Signup for codecorgi and build your dev profile</title>
+                    <meta property="og:type" content="business.business" />
+                    <meta property="og:title" content="Signup for codecorgi and build your dev profile" />
+                    <meta property="og:url" content={ `${PROD_URL}/signup` } />
+                    <meta property="og:image" content={`${femaleOctocat}` } />
+                </Helmet>
                 <div className="container inner-top-md inner-bottom-sm">
                     <div className="row">
                         <div className="col-md-5 inner-right inner-bottom-xs">
@@ -14,7 +23,7 @@ class Signup extends React.Component {
                                 <h1>Signup</h1>
                                 <p>Use github to create or access your account.</p>
                                 <p>
-                                    <Link to="/api/auth/github" className="btn btn-large">
+                                    <Link to={`${BACKEND_URL}/api/auth/github`} target="_self" className="btn btn-large">
                                         <i className="icon-github" />
                                         Github Signup
                                     </Link>
