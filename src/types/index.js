0d4b655eae4a3a89b023675d460cea16b3d52b8a
David Silva <david.silva@dokbid.com>
2018-01-07 22:03:57 -0500

    Created challenges and challenge prop type

--- src/types/index.js
+++ src/types/index.js
@@ -5,3 +5,24 @@ export const ProfileType = PropTypes.shape({
     authenticated: PropTypes.bool,
     username: PropTypes.string,
 });
+
+export const ChallengeType = PropTypes.shape({
+    number: PropTypes.string,
+    title: PropTypes.string,
+    id: PropTypes.string,
+    source: PropTypes.array,
+    tags: PropTypes.arrayOf(PropTypes.string),
+    body: PropTypes.shape({
+        short_description: PropTypes.string,
+        description: PropTypes.string,
+    }),
+    created: PropTypes.string,
+    url: PropTypes.string,
+    head: PropTypes.shape({
+        resolution: PropTypes.string,
+        status: PropTypes.string,
+        difficulty: PropTypes.string,
+    }),
+});
+
+export const ChallengesType = PropTypes.arrayOf(ChallengeType);
