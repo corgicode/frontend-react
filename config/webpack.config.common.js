51406f03bb56e8b573a4dc010af131241dc7f641
dvidsilva <contacto@dvidsilva.com>
2019-02-11 01:15:56 -0500

    created shortcuts for imports

--- config/webpack.config.common.js
+++ config/webpack.config.common.js
@@ -1,6 +1,15 @@
 const path = require('path');
 
 module.exports = {
+    resolve: {
+        alias: {
+            Components: path.join(__dirname, '/../src/components'),
+            Containers: path.join(__dirname, '/../src/containers'),
+            Images: path.join(__dirname, '/../src/assets/images'),
+            Constants: path.join(__dirname, '/../src/constants'),
+            Lib: path.join(__dirname, '/../src/lib'),
+        },
+    },
     module: {
         rules: [
             {
